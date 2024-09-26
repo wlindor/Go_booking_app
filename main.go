599a5111d0a12cce6933fd206cf32e577e01ebe3
wlindor <47373982+wlindor@users.noreply.github.com>
2024-09-25 19:50:47 -0500

    updating file path

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = conferenceTickets
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and only %v tickets left\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
 }
