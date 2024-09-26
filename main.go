e28c5f29a9d46d5764ad64050d4a538df299b16e
wlindor <wlindor@gmail.com>
2024-09-26 09:44:55 -0500

    learning arrays and slices

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = conferenceTickets
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and only %v tickets left\n", conferenceTickets, remainingTickets)
@@ -13,19 +14,28 @@ func main() {
 
 	var userName string
 	var emailAddress string
-	var userTickets int
+	var userTickets uint
+	var lastName string
 	// ask user for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&userName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 
 	fmt.Println("Enter your email address: ")
 	fmt.Scan(&emailAddress)
 
 	fmt.Println("Enter how many tickets you would like to purchase: ")
 	fmt.Scan(&userTickets)
-	fmt.Printf("Thank you %v for booking your %v tickets. You will receive a confirmation email at %v \n", userName, userTickets, emailAddress)
+	fmt.Printf("Thank you %v %v for booking your %v tickets. You will receive a confirmation email at %v \n", userName, lastName, userTickets, emailAddress)
 
-	remainingTickets = remainingTickets - uint(userTickets)
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, userName+" "+lastName)
+	fmt.Printf("We have a total of %v tickets left\n", remainingTickets)
+	fmt.Printf("Here are all of the bookings: ")
 
-	fmt.Printf("We have a total of %v tickets and only %v tickets left\n", conferenceTickets, remainingTickets)
+	for i := 0; i < len(bookings); i++ {
+		fmt.Printf(bookings[i])
+	}
 }
