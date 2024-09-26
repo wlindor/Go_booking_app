bbff8dbd6429a3f13de204e77fc7610af2e9a53a
wlindor <47373982+wlindor@users.noreply.github.com>
2024-09-25 20:42:21 -0500

    learning pointers

--- main.go
+++ main.go
@@ -3,11 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = conferenceTickets
+	var remainingTickets uint = conferenceTickets
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and only %v tickets left\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&userName)
+
+	fmt.Println("Enter how many tickets you would like to purchase: ")
+	fmt.Scan(&userTickets)
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+
+	remainingTickets = remainingTickets - uint(userTickets)
+
+	fmt.Printf("We have a total of %v tickets and only %v tickets left\n", conferenceTickets, remainingTickets)
 }
